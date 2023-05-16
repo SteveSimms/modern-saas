0ae8023d0854d5dd7520085e0b302cb5015a0e68
Steven Simms <simmsstev@gmail.com>
2023-05-15 22:21:33 -0700

    Set Up server side supabase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,16 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "$lib/supabase-types.ts";
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			supabase: SupabaseClient<Database>;
+			getSession(): Promise<Session | null>;
+		}
+		interface PageData {
+			session: Session | null;
+		}
 		// interface Platform {}
 	}
 }
--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,13 +1,13 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const getEnvironmentVariable = (environmentVariable: string): string => {
+function getEnvironmentVariable(environmentVariable: string): string {
 	const validateEnvironmentVariable = process.env[environmentVariable];
 	if (!validateEnvironmentVariable) {
 		throw new Error(`Couldn't find environment variable: ${environmentVariable}`);
 	}
 	return environmentVariable;
-};
+}
 
 export const ENV = {
 	PUBLIC_SUPABASE_ANON_KEY: getEnvironmentVariable("PUBLIC_SUPABASE_ANON_KEY"),
